3f82dcdd08615649bb76b21e6e2a1b835530eae9
rodric15 <rodrigo.fla75@gmail.com>
2021-03-31 15:47:34 -0300

    add file python_numbers

---
+++ Curso Python/05_Python_Numbers.py
@@ -0,0 +1,18 @@
+"""Type Conversion
+You can convert from one type to another with the int(), float(), and complex() methods:
+    """
+x = 1
+y = 2.0
+z = 1j
+
+print(x)
+print(y)
+print(z)
+
+a = float(x)
+b = int(y)
+c = complex(x)
+
+print(type(a))
+print(type(b))
+print(type(c))
